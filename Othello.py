6736fe1067db5c9e570a27e2ce01f2437365b24d
Lucas <lucas.c.barreto@hotmail.com>
2021-04-10 15:53:50 +0100

    Created functions to check game state and return the game results

--- Othello.py
+++ Othello.py
@@ -28,16 +28,16 @@ class Othello:
         for x in range(ROWS):
             for y in range(COLS):
                 self.board[x][y] = EMPTY
-        self.board[3][3] = PLAYER1
-        self.board[4][4] = PLAYER1
-        self.board[3][4] = PLAYER2
-        self.board[4][3] = PLAYER2
+        self.board[3][3] = PLAYER2
+        self.board[4][4] = PLAYER2
+        self.board[3][4] = PLAYER1
+        self.board[4][3] = PLAYER1
         self.player1 = PLAYER1
         self.player2 = PLAYER2
         return self.board
 
     def move(self, player, row, col):
-        disks_to_flip = self.list_valid_moves(player, row, col)
+        disks_to_flip = self.check_move(player, row, col)
 
         if disks_to_flip == 0:
             raise InvalidMoveException()
@@ -46,8 +46,8 @@ class Othello:
         for x, y in disks_to_flip:
             self.board[x][y] = player
 
-    def list_valid_moves(self, player, row, col):
-        if self.board[row][col] != EMPTY or not valid_move(row, col):
+    def check_move(self, player, row, col):
+        if self.board[row][col] != self.empty or not valid_move(row, col):
             return 0
 
         self.board[row][col] = player
@@ -87,3 +87,35 @@ class Othello:
         if len(disks_to_flip) == 0:
             return 0
         return disks_to_flip
+
+    def valid_moves(self, player):
+        valid_moves = []
+
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.check_move(player, x, y):
+                    valid_moves.append([x, y])
+        return valid_moves
+
+    def game_state(self):
+        return len(self.valid_moves(self.player1)) == 0 and len(self.valid_moves(self.player2)) == 0
+
+    def disks_count(self):
+        player1 = 0
+        player2 = 0
+        for x in range(ROWS):
+            for y in range(COLS):
+                if self.board[x][y] == self.player1:
+                    player1 += 1
+                elif self.board[x][y] == self.player2:
+                    player2 += 1
+        return {self.player1: player1, self.player2: player2}
+
+    def get_winner(self):
+        winner_count = self.disks_count()
+        if winner_count[self.player1] > winner_count[self.player2]:
+            return self.player1
+        elif winner_count[self.player1] < winner_count[self.player2]:
+            return self.player2
+        else:
+            return None
