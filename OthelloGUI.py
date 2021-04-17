386eadb8185fbb43f047d11c576fc56d32d8bf76
Lucas <lucas.c.barreto@hotmail.com>
2021-04-17 23:13:10 +0100

    Changed implementation of GUI from library tkinter to pygame

--- OthelloGUI.py
+++ OthelloGUI.py
@@ -1,36 +1,35 @@
 import Othello
-from tkinter import *
+import pygame
 
 BOARD_WIDTH = 600
 BOARD_HEIGHT = 600
 ROWS = Othello.ROWS
 COLS = Othello.COLS
-DELAY = 100
-BACKGROUND_COLOUR = 'Green'
-PLAYERS = {Othello.PLAYER1: 'Black', Othello.PLAYER2: 'White'}
+BACKGROUND_COLOUR = (0, 128, 0)
+PLAYERS = {Othello.PLAYER1: (0, 0, 0), Othello.PLAYER2: (255, 255, 255)}
 
 
 class OthelloGUI:
 
     def __init__(self):
-        self.window = Tk()
-        self.window.title("Othello")
-        self.board = Canvas(self.window, bg=BACKGROUND_COLOUR, width=BOARD_WIDTH, height=BOARD_HEIGHT)
-        self.board.pack()
-        self.window.bind("<Button-1>", self.play_again)
+        pygame.init()
+        pygame.display.set_caption('Othello')
+        self.window = pygame.display.set_mode((BOARD_HEIGHT, BOARD_WIDTH))
         self.board_state = Othello.Othello()
         self.start = False
+        self.draw_board()
 
     def draw_board(self):
-        self.board.delete(ALL)
+        self.window.fill(BACKGROUND_COLOUR)
         self.draw_lines()
         self.draw_all_disks()
+        pygame.display.update()
 
     def draw_lines(self):
         for i in range(ROWS):
-            self.board.create_line(i * BOARD_WIDTH / ROWS, 0, i * BOARD_WIDTH / ROWS, BOARD_WIDTH, )
+            pygame.draw.line(self.window, (0, 0, 0), (i * BOARD_WIDTH / ROWS, 0), (i * BOARD_WIDTH / ROWS, BOARD_WIDTH))
         for i in range(COLS):
-            self.board.create_line(0, i * BOARD_HEIGHT / COLS, BOARD_HEIGHT, i * BOARD_HEIGHT / COLS, )
+            pygame.draw.line(self.window, (0, 0, 0), (0, i * BOARD_HEIGHT / COLS), (BOARD_HEIGHT, i * BOARD_HEIGHT / COLS))
 
     def draw_all_disks(self):
         for x in range(ROWS):
@@ -39,70 +38,46 @@ class OthelloGUI:
                     self.draw_disk(x, y)
 
     def draw_disk(self, row, col):
-        self.board.create_oval(col * BOARD_WIDTH / COLS, row * BOARD_HEIGHT / ROWS,
-                               (col + 1) * BOARD_WIDTH / COLS, (row + 1) * BOARD_HEIGHT / ROWS,
-                               fill=PLAYERS[self.board_state.get_board()[row][col]], outline="")
+        pygame.draw.circle(self.window, PLAYERS[self.board_state.get_board()[row][col]],
+                           ((col + 0.51) * 600 / 8, (row + 0.51) * 600 / 8), 37)
 
     def game_over(self):
         scores = self.board_state.disks_count()
-        self.board.delete(ALL)
+        self.window.fill(BACKGROUND_COLOUR)
+        title_font = pygame.font.SysFont('Helvetica', 60, True)
+        player_font = pygame.font.SysFont('Helvetica', 40, True)
+        play_again_font = pygame.font.SysFont('Helvetica', 30, True)
 
         if self.board_state.get_winner() is None:
-            title_text = "It is a draw! \n"
-            colour_winner = "Yellow"
-        else:
-            title_text = PLAYERS[self.board_state.get_winner()] + " wins! \n"
+            title_text = title_font.render('It is a draw!', True, (255, 255, 0))
+
+        elif self.board_state.get_winner() == 'B':
             colour_winner = PLAYERS[self.board_state.get_winner()]
+            title_text = title_font.render('Black wins!', True, colour_winner)
+        else:
+            colour_winner = PLAYERS[self.board_state.get_winner()]
+            title_text = title_font.render('White wins!', True, colour_winner)
 
-        play_again_text = "Click to play new match \n"
-        player1_text = "Black \n"
-        player2_text = "White \n"
+        play_again_text = play_again_font.render('Click to play new match', True, (255, 255, 0))
+        player1_title = player_font.render('Black', True, PLAYERS['B'])
+        player1_score = player_font.render(str(scores['B']), True, PLAYERS['B'])
+        player2_title = player_font.render('White', True, PLAYERS['W'])
+        player2_score = player_font.render(str(scores['W']), True, PLAYERS['W'])
 
-        self.board.create_text(BOARD_WIDTH / 1.935,
-                               BOARD_HEIGHT / 6,
-                               font="cmr 60 bold",
-                               fill=colour_winner,
-                               text=title_text)
+        self.window.blit(title_text, title_text.get_rect(centerx=BOARD_WIDTH / 2, centery=BOARD_HEIGHT / 6))
+        self.window.blit(player1_title, player1_title.get_rect(centerx=BOARD_WIDTH / 3, centery=3 * BOARD_HEIGHT / 8))
+        self.window.blit(player2_title, player2_title.get_rect(centerx=BOARD_WIDTH / 1.5, centery=3 * BOARD_HEIGHT / 8))
+        self.window.blit(player1_score, player1_score.get_rect(centerx=BOARD_WIDTH / 3.1, centery=1 * BOARD_HEIGHT / 2))
+        self.window.blit(player2_score, player2_score.get_rect(centerx=BOARD_WIDTH / 1.5, centery=1 * BOARD_HEIGHT / 2))
+        self.window.blit(play_again_text, play_again_text.get_rect(centerx=BOARD_WIDTH / 2, centery=BOARD_HEIGHT / 1.33))
 
-        self.board.create_text(BOARD_WIDTH / 3,
-                               3 * BOARD_HEIGHT / 8,
-                               font="cmr 40 bold",
-                               fill="Black",
-                               text=player1_text)
+        pygame.display.update()
 
-        self.board.create_text(BOARD_WIDTH / 3.1,
-                               1 * BOARD_HEIGHT / 2,
-                               font="cmr 40 bold",
-                               fill="Black",
-                               text=str(scores['B']))
-
-        self.board.create_text(BOARD_WIDTH / 1.5,
-                               3 * BOARD_HEIGHT / 8,
-                               font="cmr 40 bold",
-                               fill="White",
-                               text=player2_text)
-
-        self.board.create_text(BOARD_WIDTH / 1.5,
-                               1 * BOARD_HEIGHT / 2,
-                               font="cmr 40 bold",
-                               fill="White",
-                               text=str(scores['W']))
-
-        self.board.create_text(BOARD_WIDTH / 2,
-                               BOARD_HEIGHT / 1.33,
-                               font="cmr 30 bold",
-                               fill=colour_winner,
-                               text=play_again_text)
-
-    def play_again(self, event):
+    def play_again(self):
         self.board_state.reset_board()
         self.draw_board()
 
     def update_board_state(self, new_state):
         self.board_state = new_state
-
-    def mainloop(self):
-        self.draw_board()
-
-        while True:
-            self.window.update()
+        self.draw_all_disks()
+        pygame.display.update()
